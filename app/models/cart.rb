3b8dd89dc96c9ad03b19a80b9616a430c613a01c
Ilya Zarembsky <wlycdgrfromflatiron@gmail.com>
2018-09-04 15:11:14 -0400

    Update and complete Cart model definition

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,13 +2,14 @@ class Cart < ApplicationRecord
   belongs_to :user
   has_many :cart_items
   has_many :items, through: :cart_items
-  has_one :cart_store
-  has_one :store, through: :cart_store
+  has_one :store_location
+  has_one :store, through: :store_location
 
   def total
     total = 0
-    items.each do |item|
-      total += item.price
+    # refactor to use Array#reduce, if available
+    cart_items.each do |cart_item|
+      total += cart_item.price
     end
     total
   end
