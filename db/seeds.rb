7813b004d54aa9a67f386ea9459da7afe25c0930
wlycdgrfromflatiron <wlycdgrfromflatiron@gmail.com>
2018-09-06 18:04:46 -0400

    Add tag creation and some tags to db seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,6 +24,26 @@ def create_prefab_stores(user_name, names)
   end
 end
 
+def create_tag(name, users_and_prefabs)
+  tag = Tag.create({name: name})
+
+  users_and_prefabs.each do |user_and_prefabs|
+    user = User.find_by(name: user_and_prefabs[0])
+
+    tag.users << user
+
+    user_and_prefabs[1].each do |prefab_store_name|
+      tag.prefab_stores << user.prefab_stores.find_by(name: prefab_store_name)
+    end
+
+    user_and_prefabs[2].each do |prefab_item_name|
+      tag.prefab_items << user.prefab_items.find_by(name: prefab_item_name)
+    end
+  end
+
+  tag.save
+end
+
 User.create([
   make_user_hash('ilya'), # id 1
   make_user_hash('wlycdgr'), #id 2
@@ -34,4 +54,28 @@ create_prefab_stores('ilya', [
   'Best Buy Union Square',
   'The Strand',
   'Amazon'
+])
+
+create_tag('NYC', [
+  ['ilya', ['Best Buy Union Square', 'The Strand'], []]
+])
+
+create_tag('favorite', [
+  ['ilya', ['The Strand', 'Amazon'], []]
+])
+
+create_tag('online', [
+  ['ilya', ['Amazon'], []]
+])
+
+create_tag('bookstore', [
+  ['ilya', ['The Strand'], []]
+])
+
+create_tag('independent', [
+  ['ilya', ['The Strand'], []]
+])
+
+create_tag('24hr', [
+  ['ilya', ['Best Buy Union Square'], []]
 ])
