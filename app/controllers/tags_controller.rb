914433e5491d46024e63111ae249dee36511c9b9
Ilya Zarembsky <wlycdgrfromflatiron@gmail.com>
2018-09-06 00:24:21 -0400

    Implement Tags#create

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,12 +1,10 @@
 class TagsController < SessionsController
   before_action {|c| c.session_guard c.this_user_nested? }
 
-  #index - show all tags: name, link to details, link to delete
   def index
     @tags = @user.tags
   end
 
-  #new - give name, select prefab items and prefab stores to add the tag to
   def new
     @tag = Tag.new
     @prefab_stores = @user.prefab_stores
@@ -15,10 +13,18 @@ class TagsController < SessionsController
 
   #edit (doubles as #show) - name, all the user's prefab items and stores that use it
   def edit
+    render html: "This is where we edit the tag woot!"
   end
 
   #create - make and save the new tag! and, add it to the indicated stores and items
   def create
+    tag = Tag.new(tag_params)
+
+    if tag.save
+      redirect_to edit_user_tag_path(@user.id, tag.id)
+    else
+      render :new
+    end
   end
 
   #update - do the #edit changes
@@ -27,5 +33,11 @@ class TagsController < SessionsController
 
   #delete - confirm that user is ok with all prefabs, cart stores, and cart items losing this tag
   def delete
+  end
+
+  private
+
+  def tag_params
+    params.require(:tag).permit(:name, prefab_store_ids: [], prefab_item_ids: [])
   end
 end
