dc2f766bdfc43f7abf6cf7646ba86e98e6e7bc95
Ilya Zarembsky <wlycdgrfromflatiron@gmail.com>
2018-09-05 23:47:12 -0400

    Finish implementing check box collection display helper. Implement labeled text field display helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,17 @@
 module ApplicationHelper
   def check_boxes_for(collection, names, title, fbo)
     content = "<h3>#{title}</h3>"
-    #fbo.collection_check_boxes(names, collection, :id, :name) do |b|
-    #  <p>
-    #    b.label { b.check_box + b.text }
-    #  </p>
-    #end
+    fbo.collection_check_boxes(names, collection, :id, :name) do |b|
+      content << "<p>"
+        content << "#{b.label { b.check_box + b.text }}"
+      content << "</p>"
+    end
+    content
+  end
+
+  def labeled_text_field(fbo, attribute)
+    content = "#{fbo.label attribute}"
+    content << "#{fbo.text_field attribute}"
+    content
   end
 end
