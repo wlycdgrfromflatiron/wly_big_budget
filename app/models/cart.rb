d1889cd48487d3159d2c008bff2b9fb80cf2d6b8
wlycdgrfromflatiron <wlycdgrfromflatiron@gmail.com>
2018-09-07 17:45:27 -0400

    Add accepts_nested_attributes_for macros to Cart for CartItem and CartStore

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -4,6 +4,8 @@ class Cart < ApplicationRecord
   has_many :prefab_items, through: :cart_items
   has_one :cart_store
   has_one :prefab_store, through: :cart_store
+  accepts_nested_attributes_for :cart_items
+  accepts_nested_attributes_for :cart_store
 
   def total
     cart_items.reduce(0) { |total, cart_item| total += cart_item.price }
