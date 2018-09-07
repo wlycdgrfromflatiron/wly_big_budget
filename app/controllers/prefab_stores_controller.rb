4ace6ee5512edf15af0fd2468bfbc03aa0379fce
wlycdgrfromflatiron <wlycdgrfromflatiron@gmail.com>
2018-09-06 22:06:41 -0400

    Implement #create and #destroy for PrefabStores

--- app/controllers/prefab_stores_controller.rb
+++ app/controllers/prefab_stores_controller.rb
@@ -1,6 +1,6 @@
 class PrefabStoresController < NestedResourcesController
   before_action {|c| c.session_guard c.this_user_nested? }
-  before_action :load_prefab_store, only: [:edit, :update, :delete]
+  before_action :load_prefab_store, only: [:edit, :update, :destroy]
 
   def index
     @prefab_stores = @user.prefab_stores
@@ -13,11 +13,12 @@ class PrefabStoresController < NestedResourcesController
 
   # doubles as #show
   def edit
+    render html: "This is where we edit the thing!"
   end
 
   def create
     prefab_store = PrefabStore.new(prefab_store_params)
-    prefab_store.users << @user
+    prefab_store.user = @user
     if prefab_store.save
       redirect_to edit_user_prefab_store_path(@user, prefab_store)
     else
@@ -37,22 +38,20 @@ class PrefabStoresController < NestedResourcesController
   end
 
   def destroy
-    prefab_store.destroy
+    @prefab_store.destroy
     redirect_to user_prefab_stores_path
   end
 
   private
 
   def prefab_store_params
-    params.require(:prefab_store).permit(:name, tags: [])
+    params.require(:prefab_store).permit(:name, tag_ids: [])
   end
 
   def load_prefab_store
-    prefab_store = @user.prefab_stores.find_by(id: params[:id])
-    if !prefab_store
+    @prefab_store = @user.prefab_stores.find_by(id: params[:id])
+    if !@prefab_store
       redirect_to user_prefab_stores_path
-    else
-      @prefab_store = prefab_store
     end
   end
 end
