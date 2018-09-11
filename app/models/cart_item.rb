79e4705b6c8f9762d7a5b38770beed2b22426a18
wlycdgrfromflatiron <wlycdgrfromflatiron@gmail.com>
2018-09-11 00:28:47 -0400

    Update TODO. Make prefab_item optional on cart_item, allowing users to create carts with items that have no prefab items

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,6 +1,6 @@
 class CartItem < ApplicationRecord
   belongs_to :cart
-  belongs_to :prefab_item
+  belongs_to :prefab_item, optional: true
   has_many :cart_item_tags
   has_many :tags, through: :cart_item_tags
 end
