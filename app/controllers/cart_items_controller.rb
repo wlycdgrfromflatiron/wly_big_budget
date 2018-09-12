8be5087c83d058f38f0e45bf1c1a989fe8ad9a78
wlycdgrfromflatiron <wlycdgrfromflatiron@gmail.com>
2018-09-11 23:00:41 -0400

    Modify add new cart item form to account for the fact that it's now rendered from its own CartItemsController

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,10 +1,10 @@
 class CartItemsController < SessionsController
     before_action {|c| c.session_guard c.this_carts_user? }
     before_action :load_cart
-    before_action :load_prefabs_and_tags, only: [:additem, :new, :create, :edit]
-    before_action :load_user_cart, only: [:additem, :edit, :update, :destroy]
+    before_action :load_prefab_items_and_tags, only: [:new, :create]
 
     def new
+        @cart_item = CartItem.new
     end
 
     def create
@@ -13,8 +13,7 @@ class CartItemsController < SessionsController
     def destroy
     end
 
-    def load_prefabs_and_tags
-        @prefab_stores = @user.prefab_stores
+    def load_prefab_items_and_tags
         @prefab_items = @user.prefab_items
         @tags = @user.tags
     end
