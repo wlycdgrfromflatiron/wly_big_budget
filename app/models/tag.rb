2bb26ea9f7c1e99663eee1677ca65cf23855e2dd
wlycdgrfromflatiron <wlycdgrfromflatiron@gmail.com>
2018-09-13 23:38:29 -0400

    Implement Tag#cart_items_directly_or_through_prefab

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -9,4 +9,15 @@ class Tag < ApplicationRecord
   has_many :prefab_items, through: :prefab_item_tags
   has_many :prefab_store_tags
   has_many :prefab_stores, through: :prefab_store_tags
+
+  def cart_items_directly_or_through_prefab_items
+    cart_items_through_prefab_items = prefab_items.collect do |prefab_item|
+      prefab_item.cart_items
+    end
+    cart_items_through_prefab_items.flatten!
+
+    direct_cart_items = cart_items 
+
+    direct_cart_items.concat(cart_items_through_prefab_items).uniq!
+  end
 end
