9d2302ce6dece6c684ba5e683b3c0cdb1ade6f01
wlycdgrfromflatiron <wlycdgrfromflatiron@gmail.com>
2018-10-06 04:32:07 -0400

    Disable and remove Turbolinks. Introduce javascript handler to intercept Tags navbar link click

--- app/assets/javascripts/tags.js
+++ app/assets/javascripts/tags.js
@@ -1,1 +1,13 @@
-const bananas = "FOSTER"
+(function TagsIndex(){
+    function handleTagsNavbarLinkClick(event){
+        event.preventDefault();
+
+        console.log("tags navbar link clicked!");
+    }
+
+    window.onload = () => {
+        const tagsNavbarLink = document.getElementById('tags_navbar_link');
+
+        tagsNavbarLink.addEventListener('click', handleTagsNavbarLinkClick);
+    }
+})();
