65f9e034d15bd1e93a00908a5de420f6bbf5ad61
Ilya Zarembsky <wlycdgrfromflatiron@gmail.com>
2018-10-05 23:55:45 -0400

    Add presence validation and email format validation to sign in form through html attributes. Remove unnecessary javascript.

--- app/assets/javascripts/home.js
+++
@@ -1,7 +0,0 @@
-window.onload = () => {
-    if (document.getElementsByTagName('body')[0].className === "frontPage"){
-        // block form submission if password is missing
-        // block form submission is email is missing
-        // submit through AJAX
-    }
-}
