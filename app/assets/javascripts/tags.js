ad7e21d84ac8e5a2eb2da78c93e30e7bfc3c3509
Ilya Zarembsky <wlycdgrfromflatiron@gmail.com>
2018-10-20 00:10:28 -0400

    Convert tags on tag index to links

--- app/assets/javascripts/tags.js
+++ app/assets/javascripts/tags.js
@@ -1,37 +1,40 @@
-(function TagsIndex() {
-    function handleTagsNavbarLinkClick(event){
-        event.preventDefault();
+function handleTagsNavbarLinkClick(event){
+    event.preventDefault();
 
-        console.log("tags navbar link clicked!");
-        console.log(`userId: ${userId}`);
+    console.log("tags navbar link clicked!");
+    console.log(`userId: ${userId}`);
 
-        // change URL - use pushState
-        history.pushState({}, "Tags", `/users/${userId}/tags`)
+    // change URL - use pushState
+    history.pushState({}, "Tags", `/users/${userId}/tags`)
 
-        // update to show "LOADING"
-        const mainDiv = document.getElementById('main-content-column')
-        mainDiv.innerHTML = "LOADING TAGS...."
+    // update to show "LOADING"
+    const mainDiv = document.getElementById('main-content-column')
+    mainDiv.innerHTML = "LOADING TAGS...."
 
-        // send data request
-        // when data comes back, parse it and insert it
-        fetch(`/users/${userId}/tags.json`)
-            .then(response => response.json())
-            .then(responseJSON => {
-                console.log(responseJSON)
-                mainDiv.innerHTML = HandlebarsTemplates["tags/index"]({
-                    header: "ALL THE TAGS!",
-                    tags: responseJSON
-                })
+    // send data request
+    // when data comes back, parse it and insert it
+    fetch(`/users/${userId}/tags.json`)
+        .then(response => response.json())
+        .then(responseJSON => {
+            console.log(responseJSON)
+            mainDiv.innerHTML = HandlebarsTemplates["tags/index"]({
+                header: "ALL THE TAGS!",
+                tags: responseJSON
             })
+        })
+}
+
+function handleTagClick(event){
+    event.preventDefault();
+    console.log("Tag clicked!");
+}
+
+window.addEventListener('load', () => {
+    console.log("TagsIndex window.onLoad handler called")
+
+    const tagsNavbarLink = document.getElementById('tags-navbar-link');
+    tagsNavbarLink.addEventListener('click', handleTagsNavbarLinkClick);
+    if (window.location.pathname.match(/users\/\d\/tags$/)){
+        tagsNavbarLink.dispatchEvent(new Event('click'));
     }
-
-    window.addEventListener('load', () => {
-        console.log("TagsIndex window.onLoad handler called")
-
-        const tagsNavbarLink = document.getElementById('tags-navbar-link');
-        tagsNavbarLink.addEventListener('click', handleTagsNavbarLinkClick);
-        if (window.location.pathname.match(/users\/\d\/tags$/)){
-            tagsNavbarLink.dispatchEvent(new Event('click'));
-        }
-    })
-})();
+})
