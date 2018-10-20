ef79d8a4ab461c5c02791bd7c1e91f5ebe1374b4
Ilya Zarembsky <wlycdgrfromflatiron@gmail.com>
2018-10-20 13:18:04 -0400

    Attach class method listener to tag index links

--- app/assets/javascripts/tags.js
+++ app/assets/javascripts/tags.js
@@ -2,6 +2,7 @@ class TagsController {
     constructor(){
         console.log("TagIndexController constructor entered")
 
+        this.handleTagClick = this.handleTagClick.bind(this)
         this.handleTagsNavbarLinkClick = this.handleTagsNavbarLinkClick.bind(this)
 
         const tagsNavbarLink = document.getElementById('tags-navbar-link');
@@ -10,31 +11,43 @@ class TagsController {
         if (window.location.pathname.match(/users\/\d\/tags$/)){
             tagsNavbarLink.dispatchEvent(new Event('click'));
         }
-    }    
+    }   
     
-    async fetchStoreAndRenderTags(){
-        let tags = await fetch(`/users/${userId}/tags.json`)
-        store.tags = await tags.json()
-        this.renderStoredTags(this.sortTags(SORT_ALPHA_ASC))
+    // HANDLERS - need to be bound to class instance in constructor
+    handleTagClick(event){ // user clicks a tag in the tag index view
+        event.preventDefault();
+        const detailsDiv = document.getElementById('tag-details')
+        const selectedTagName = detailsDiv.getElementsByTagName('h3')[0]
+        selectedTagName.innerHTML = event.target.innerHTML;
     }
 
-    handleTagsNavbarLinkClick(event){
+    handleTagsNavbarLinkClick(event){ // user clicks the T in the navbar
         console.log("tags navbar link clicked!");
         console.log(`userId: ${userId}`);
 
         event.preventDefault();
         history.pushState({}, "Tags", `/users/${userId}/tags`)
         store.tags ? 
-            this.renderStoredTags(sortTags(SORT_ALPHA_ASC)) : 
+            this.renderStoredTags(this.sortTags(SORT_ALPHA_ASC)) : 
             this.fetchStoreAndRenderTags();
+    }
+
+    async fetchStoreAndRenderTags(){
+        let tags = await fetch(`/users/${userId}/tags.json`)
+        store.tags = await tags.json()
+        this.renderStoredTags(this.sortTags(SORT_ALPHA_ASC))
     }
 
     renderStoredTags(sortedTags){
         const mainDiv = document.getElementById('main-content-column')
         mainDiv.innerHTML = HandlebarsTemplates["tags/index"]({
             tags: sortedTags,
-            selectedTag: sortedTags[0]
+            selectedTag: sortedTags[0],
         })
+        const tagLinks = document.getElementsByClassName('tag-index-link');
+        for (const tagLink of tagLinks){
+            tagLink.addEventListener('click', event => this.handleTagClick(event))
+        }
     }
 
     sortTags(sort){
@@ -51,12 +64,5 @@ class TagsController {
                 sortFunc = (tagA, tagB) => tagA.name.localeCompare(tagB.name);
         }
         return sortedTags.sort(sortFunc)
-    }
-
-    handleTagClick(event){
-        event.preventDefault();
-        const detailsDiv = document.getElementById('tag-details')
-        const selectedTagName = detailsDiv.getElementsByTagName('h3')[0]
-        selectedTagName.innerHTML = event.target.innerHTML;
     }
 }
