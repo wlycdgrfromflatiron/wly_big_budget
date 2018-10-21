be699f0c432ba17cf87a69931d062212e0bbdf73
Ilya Zarembsky <wlycdgrfromflatiron@gmail.com>
2018-10-21 19:22:29 -0400

    Add total spent to tag serializer. Add it to tag index and show views

--- app/serializers/tag_serializer.rb
+++ app/serializers/tag_serializer.rb
@@ -1,5 +1,5 @@
 class TagSerializer < ActiveModel::Serializer
-  attributes :id, :name, :carts
+  attributes :id, :name, :carts, :total_spent
   has_many :prefab_stores
   has_many :prefab_items
   # has_many :prefab_item_tags
