45583b8c21a6a6d30ede46be8ac25e54db83bdc4
Ilya Zarembsky <wlycdgrfromflatiron@gmail.com>
2018-11-08 16:15:36 -0500

    Remove commented out bits from Tag serializer. Minify id attribute key

--- app/serializers/tag_serializer.rb
+++ app/serializers/tag_serializer.rb
@@ -1,8 +1,4 @@
 class TagSerializer < ActiveModel::Serializer
-  attribute :id
+  attribute :id, key: :x
   attribute :name, key: :n
-  #attribute :carts, key: :c
-  # attribute :total_spent, key: :t
-  #has_many :prefab_stores, key: :s
-  #has_many :prefab_items, key: :i
 end
